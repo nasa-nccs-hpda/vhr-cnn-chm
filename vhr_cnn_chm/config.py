0f468591bc2cfbe7c5d2a250a43cf7f172ed0cb9
jordancaraballo <jordan.a.caraballo-vega@nasa.gov>
2022-11-16 12:59:18 -0500

    Removing crop option model configuration file

--- vhr_cnn_chm/config.py
+++ vhr_cnn_chm/config.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Optional
 from dataclasses import dataclass, field
 from tensorflow_caney.config.cnn_config import Config
 
@@ -7,40 +7,40 @@ from tensorflow_caney.config.cnn_config import Config
 class CHMConfig(Config):
 
     # Directory where atl08 training data is stored
-    atl08_dir: str = 'input'
+    atl08_dir: Optional[str] = 'input'
 
     # Start year for atl08 training data
-    atl08_start_year: int = 2018
+    atl08_start_year: Optional[int] = 2018
 
     # End year for atl08 training data
-    atl08_end_year: int = 2022
+    atl08_end_year: Optional[int] = 2022
 
     # Label attribute to choose from atl08 dataset
-    label_attribute: str = 'h_can_20m'
+    label_attribute: Optional[str] = 'h_can_20m'
 
     # General CRS for the study area, variable between UTM zones
-    general_crs: str = 'EPSG:32628'
+    general_crs: Optional[str] = 'EPSG:32628'
 
     # Regex to find WorldView data
-    wv_data_regex: List[str] = field(default_factory=lambda: [])
+    wv_data_regex: Optional[List[str]] = field(default_factory=lambda: [])
 
     # Directory where intermediary mask lives in
-    mask_dir: str = 'trees'
+    mask_dir: Optional[str] = 'trees'
 
     # Directory where cloud mask lives in
-    cloudmask_dir: str = 'clouds'
+    cloudmask_dir: Optional[str] = 'clouds'
 
     # Directory where landcover mask lives in
-    landcover_dir: str = 'landcover'
+    landcover_dir: Optional[str] = 'landcover'
 
     # Perform preprocessing with mask
-    mask_preprocessing: bool = True
+    mask_preprocessing: Optional[bool] = True
 
     # Perform postprocessing with mask
-    mask_postprocessing: bool = False
+    mask_postprocessing: Optional[bool] = False
 
     # Define if we need to create footprint of WV and ICESAT-2 matches
-    footprint: bool = True
+    footprint: Optional[bool] = True
 
     # Define if we need to generate tiles during preprocessing
-    tiles: bool = True
+    tiles: Optional[bool] = True
