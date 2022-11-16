1c42544a0f8f686c91af396c76fdf80fd6298435
jordancaraballo <jordan.a.caraballo-vega@nasa.gov>
2022-11-16 08:54:10 -0500

    Adding documentation for config class

--- vhr_cnn_chm/config.py
+++ vhr_cnn_chm/config.py
@@ -6,16 +6,41 @@ from tensorflow_caney.config.cnn_config import Config
 @dataclass
 class CHMConfig(Config):
 
+    # Directory where atl08 training data is stored
     atl08_dir: str = 'input'
+
+    # Start year for atl08 training data
     atl08_start_year: int = 2018
+
+    # End year for atl08 training data
     atl08_end_year: int = 2022
-    general_crs: str = 'EPSG:32628'
-    wv_data_regex: List[str] = field(default_factory=lambda: [])
-    mask_dir: str = 'trees'
-    cloudmask_dir: str = 'clouds'
-    mask_preprocessing: bool = True
-    mask_postprocessing: bool = False
-    crop: bool = True
-    footprint: bool = True
-    tiles: bool = True
+
+    # Label attribute to choose from atl08 dataset
     label_attribute: str = 'h_can_20m'
+
+    # General CRS for the study area, variable between UTM zones
+    general_crs: str = 'EPSG:32628'
+
+    # Regex to find WorldView data
+    wv_data_regex: List[str] = field(default_factory=lambda: [])
+
+    # Directory where intermediary mask lives in
+    mask_dir: str = 'trees'
+
+    # Directory where cloud mask lives in
+    cloudmask_dir: str = 'clouds'
+
+    # Directory where landcover mask lives in
+    landcover_dir: str = 'landcover'
+
+    # Perform preprocessing with mask
+    mask_preprocessing: bool = True
+
+    # Perform postprocessing with mask
+    mask_postprocessing: bool = False
+
+    # Define if we need to create footprint of WV and ICESAT-2 matches
+    footprint: bool = True
+
+    # Define if we need to generate tiles during preprocessing
+    tiles: bool = True
